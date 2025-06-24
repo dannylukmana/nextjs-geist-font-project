f4547801f80544d3fc878ebf676777a8b4e130a0
Danny Lukmana <47197859+dannylukmana@users.noreply.github.com>
2025-06-24 10:47:15 +0700

    Add app/src/main/java/com/example/virtualtour/Utils.kt 1750736835408

---
+++ app/src/main/java/com/example/virtualtour/Utils.kt
@@ -0,0 +1,16 @@
+package com.example.virtualtour
+
+import kotlin.math.*
+
+object Haversine {
+    fun distance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
+        val R = 6371 // Earth's radius in kilometers
+        val dLat = Math.toRadians(lat2 - lat1)
+        val dLon = Math.toRadians(lon2 - lon1)
+        val a = sin(dLat / 2).pow(2.0) +
+                cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) *
+                sin(dLon / 2).pow(2.0)
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return R * c
+    }
+}
